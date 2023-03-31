41ea46ffd4b5ae9bc213bcf47bce003a4fc97767
minhthong582000 <55283557+minhthong582000@users.noreply.github.com>
2023-03-31 16:11:01 +0700

    added mimir example
    Signed-off-by: minhthong582000 <55283557+minhthong582000@users.noreply.github.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	_ = godotenv.Load("sample_file.env")
 
-	err := client.HttpClient(os.Getenv("CLIENT_ADDR"), os.Getenv("BIND_ADDR"))
+	err := client.HttpClient(os.Getenv("CLIENT_BIND_ADDR"), os.Getenv("SERVER_BIND_ADDR"))
 	if err != nil {
 		fmt.Println(err)
 	}
