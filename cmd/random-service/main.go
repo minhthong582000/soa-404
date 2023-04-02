1b2ba731548bda7920efe9be07c0896eeab8a402
minhthong582000 <55283557+minhthong582000@users.noreply.github.com>
2023-04-02 18:43:54 +0700

    chore
    Signed-off-by: minhthong582000 <55283557+minhthong582000@users.noreply.github.com>

--- cmd/random-service/main.go
+++ cmd/random-service/main.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/minhthong582000/soa-404/internal/app/random"
 	"github.com/minhthong582000/soa-404/internal/server"
 	"github.com/minhthong582000/soa-404/pkg/config"
 	"github.com/minhthong582000/soa-404/pkg/log"
-	"github.com/minhthong582000/soa-404/pkg/tracing"
-	"go.opentelemetry.io/otel"
 )
 
 func main() {
@@ -30,31 +27,7 @@ func main() {
 		return
 	}
 
-	// Tracing
-	tracer, err := tracing.TracerFactory(tracing.OLTP, tracing.TracerConfig{
-		ServiceName:  config.Server.Name,
-		CollectorURL: config.Tracing.OLTPTracing.CollectorAddr,
-		Insecure:     config.Tracing.OLTPTracing.Insecure,
-	})
-	if err != nil {
-		logger.Errorf("TracerFactory Error: %s", err)
-	}
-	cleanup, err := tracer.InitTracer()
-	if err != nil {
-		logger.Errorf("InitTracer Error: %s", err)
-	}
-	defer cleanup(ctx)
-	tp := otel.Tracer(config.Server.Name)
-
-	randomServer := random.NewServer(
-		logger,
-		tp,
-		random.NewService(
-			tp,
-			random.NewRepository(tp),
-		),
-	)
-	server := server.New(logger, ctx, config, randomServer)
+	server := server.New(logger, ctx, config)
 
 	err = server.Run()
 	if err != nil {
--- internal/server/server.go
+++ internal/server/server.go
@@ -16,8 +16,10 @@ import (
 	interceptors "github.com/minhthong582000/soa-404/pkg/interceptor"
 	"github.com/minhthong582000/soa-404/pkg/log"
 	metric "github.com/minhthong582000/soa-404/pkg/metrics"
+	"github.com/minhthong582000/soa-404/pkg/tracing"
 
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
 
 	pb "github.com/minhthong582000/soa-404/api/v1/pb/random"
 	"github.com/minhthong582000/soa-404/internal/app/random"
@@ -28,22 +30,19 @@ type Server struct {
 	config *config.Config
 	ctx    context.Context
 	logger log.ILogger
-
-	randomServer *random.RandomServer
 }
 
 // New returns a new server.
-func New(logger log.ILogger, ctx context.Context, config *config.Config, randomServer *random.RandomServer) *Server {
+func New(logger log.ILogger, ctx context.Context, config *config.Config) *Server {
 	return &Server{
-		config:       config,
-		ctx:          ctx,
-		randomServer: randomServer,
-		logger:       logger,
+		config: config,
+		ctx:    ctx,
+		logger: logger,
 	}
 }
 
 // Run runs server.
-func (s *Server) Run() error {
+func (s Server) Run() error {
 	lis, err := net.Listen("tcp", s.config.Server.BindAddr)
 	if err != nil {
 		return fmt.Errorf("failed to listen: %v", err)
@@ -60,12 +59,26 @@ func (s *Server) Run() error {
 		s.config.Server.Name,
 	)
 
+	// Tracing
+	tracer, err := tracing.TracerFactory(tracing.OLTP, tracing.TracerConfig{
+		ServiceName:  s.config.Server.Name,
+		CollectorURL: s.config.Tracing.OLTPTracing.CollectorAddr,
+		Insecure:     s.config.Tracing.OLTPTracing.Insecure,
+	})
+	if err != nil {
+		s.logger.Errorf("TracerFactory Error: %s", err)
+	}
+	cleanup, err := tracer.InitTracer()
+	if err != nil {
+		s.logger.Errorf("InitTracer Error: %s", err)
+	}
+	tp := otel.Tracer(s.config.Server.Name)
+
 	// Register logs & metrics interceptor
 	in := interceptors.NewInterceptorManager(s.logger, metrics)
-
 	grpcServer := grpc.NewServer(
 		grpc.UnaryInterceptor(in.Logger),
-		// grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()),
+		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()),
 		grpc.ChainUnaryInterceptor(
 			in.Metrics,
 			grpc_prometheus.UnaryServerInterceptor,
@@ -76,7 +89,15 @@ func (s *Server) Run() error {
 	)
 
 	// Random service
-	pb.RegisterRandomServiceServer(grpcServer, s.randomServer)
+	randomServer := random.NewServer(
+		s.logger,
+		tp,
+		random.NewService(
+			tp,
+			random.NewRepository(tp),
+		),
+	)
+	pb.RegisterRandomServiceServer(grpcServer, randomServer)
 
 	// graceful shutdown
 	c := make(chan os.Signal, 1)
@@ -86,6 +107,7 @@ func (s *Server) Run() error {
 			// sig is a ^C, handle it
 			s.logger.Info("shutting down gRPC server...")
 			grpcServer.GracefulStop()
+			err = cleanup(s.ctx)
 			if err != nil {
 				s.logger.Errorf("trace cleanup error: %s", err)
 			}
