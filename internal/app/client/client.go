11ea36ef2c45a700d7f2f2884054caecba38ec8c
minhthong582000 <55283557+minhthong582000@users.noreply.github.com>
2024-11-19 00:07:05 +0700

    feat: prepare for oidc authentication with dex
    Signed-off-by: minhthong582000 <55283557+minhthong582000@users.noreply.github.com>

--- internal/app/client/client.go
+++ internal/app/client/client.go
@@ -41,6 +41,10 @@ func (c Client) GetRandNumber(ctx context.Context, seed int64) (int64, error) {
 	return reply.Number, nil
 }
 
+func registerDexHandler(router *echo.Echo) {
+	return
+}
+
 // HttpClient runs the client.
 func HttpClient(config *config.Config) error {
 	// Tracing
@@ -109,6 +113,10 @@ func HttpClient(config *config.Config) error {
 			"number": randNum,
 		})
 	})
+	if config.Authentication.Enabled { // Register Dex authentication handler
+		registerDexHandler(router)
+	}
+
 	if err := router.Start(config.Client.BindAddr); err != nil {
 		log.Fatal(err)
 	}
