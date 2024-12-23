1f3d78bba431380d5e022f4222728db0bcf2df80
Le Minh Thong <55283557+minhthong582000@users.noreply.github.com>
2024-12-23 23:03:17 +0700

    chore

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -41,19 +41,13 @@ const (
 
 // Logger is a logger that supports log levels, context and structured logging.
 type Logger interface {
-	// With returns a logger based off the root logger and decorates it with the given context and arguments.
+	// With returns a logger based on the root logger and decorates it with the given context and arguments.
 	With(ctx context.Context, args ...interface{}) Logger
-	// Debug uses fmt.Sprint to construct and log a message at DEBUG level
 	Debug(args ...interface{})
-	// Info uses fmt.Sprint to construct and log a message at INFO level
 	Info(args ...interface{})
-	// Error uses fmt.Sprint to construct and log a message at ERROR level
 	Error(args ...interface{})
-	// Debugf uses fmt.Sprintf to construct and log a message at DEBUG level
 	Debugf(format string, args ...interface{})
-	// Infof uses fmt.Sprintf to construct and log a message at INFO level
 	Infof(format string, args ...interface{})
-	// Errorf uses fmt.Sprintf to construct and log a message at ERROR level
 	Errorf(format string, args ...interface{})
 }
 
